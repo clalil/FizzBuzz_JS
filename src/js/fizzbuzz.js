c0f6b2a1aba37c21657b57342c4c9205ea5d79a3
clalil <cliljander@gmail.com>
2019-09-14 19:17:37 +0200

    Remove uncommented, old code.

--- src/js/fizzbuzz.js
+++ src/js/fizzbuzz.js
@@ -1,16 +1,3 @@
-// function FizzBuzz() {
-//     this.check = (number) => {
-//         if (number % 15 === 0) {
-//             return 'FizzBuzz';
-//         } else if (number % 5 === 0) {
-//             return 'Buzz';
-//         } else if (number % 3 === 0) {
-//             return 'Fizz';
-//         } else {
-//             return number
-//         }
-//     }
-// };
 
 function FizzBuzz() {
     this.check = (number) => {
