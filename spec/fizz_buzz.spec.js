559c4fe372ae3c38f27006785646909b2b2736a1
clalil <cliljander@gmail.com>
2019-09-14 08:53:55 +0200

    Add specs for 'Fizz' and corresponding method, all tests are green

--- spec/fizz_buzz.spec.js
+++ spec/fizz_buzz.spec.js
@@ -7,4 +7,9 @@ describe('FizzBuzz', () => {
     it('returns a number if no game rules are met', () => {
         expect(fizzBuzz.check(1)).to.eql(1)
     })
+
+    it('returns Fizz if the number is divisible by 3', () => {
+        expect(fizzBuzz.check(3)).to.eql('Fizz')
+    })
+    
 })
